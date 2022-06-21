a9ff3953d838d2385ca6ff10b1ad69109c74b58e
Nikola Zivkovic <nzivkovic7@gmail.com>
2022-06-21 17:24:24 +0200

    Final Selenium Project - Updated: Cities Page; Added: AdminCities Tests

--- itbootcamp_final_project/src/main/java/pages/CitiesPage.java
+++ itbootcamp_final_project/src/main/java/pages/CitiesPage.java
@@ -1,6 +1,9 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
@@ -12,6 +15,59 @@ public class CitiesPage {
     public CitiesPage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
         this.wait = wait;
+    }
+
+    public WebElement getNewItemButton(){
+        return driver.findElement(By.className("btnNewItem"));
+    }
+
+    public WebElement getSearchInput(){
+        return driver.findElement(By.id("search"));
+    }
+
+    public void waitForEditPopUpToAppear(){
+        this.wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.className("dlgNewEditItem")));
+    }
+    public void waitForEditPopUpToDisappear(){
+        this.wait.until(ExpectedConditions
+                .invisibilityOfElementLocated(By.className("dlgNewEditItem")));
+    }
+
+    public WebElement getNewItemInput(){
+        return driver.findElement(By.id("name"));
+    }
+
+    public void waitForDeletePopUpToAppear(){
+        wait.until(ExpectedConditions
+                .visibilityOfElementLocated(
+                        By.xpath("//*[text() = 'Do you really want to delete this item?']")));
+    }
+
+    public WebElement getSaveButtonInEditPopUp(){
+        return driver.findElement(By.className("btnSave"));
+    }
+
+    public WebElement getDeleteButtonInDeletePopUp(){
+        return driver.findElement(By.xpath("//span[contains(text(), ' Delete')]"));
+    }
+
+    public void waitForNumOfRowsToBe(int numOfRows) {
+        wait.until(ExpectedConditions
+                .numberOfElementsToBe(By.tagName("tr"), numOfRows + 1));
+    }
+
+    public WebElement getCellFromRow (int row, int cell){
+        return driver.findElement(By
+                .xpath("//tbody/tr[" + row + "]/td[" + (cell + 1) + "]"));
+    }
+
+    public WebElement getEditButtonFromRow (int row){
+        return driver.findElement(By.xpath("//tbody/tr[" + row + "]/td/div/button[@id = 'edit']"));
+    }
+
+    public WebElement getDeleteButtonFromRow (int row){
+        return driver.findElement(By.xpath("//tbody/tr[" + row + "]/td/div/button[@id = 'delete']"));
     }
 
 
---
+++ itbootcamp_final_project/src/main/java/tests/AdminCitiesTests.java
@@ -0,0 +1,128 @@
+package tests;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AdminCitiesTests extends BasicTest{
+
+    @Test(priority = 10)
+    public void visitsTheAdminCitiesPageAndListCities() {
+        String email = "admin@admin.com";
+        String password = "12345";
+
+        navPage.getLoginButton().click();
+        loginPage.getEmailInput().sendKeys(email);
+        loginPage.getPasswordInput().sendKeys(password);
+        loginPage.getLoginButton().click();
+
+        navPage.getAdminLink().click();
+        navPage.getCitiesLink().click();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("/admin/cities"),
+                "Current URL does not contain '/admin/cities' route");
+    }
+
+    @Test(priority = 20)
+    public void checksInputTypesForCreate_EditNewCity(){
+        navPage.getAdminLink().click();
+        navPage.getCitiesLink().click();
+
+        citiesPage.getNewItemButton().click();
+        citiesPage.waitForEditPopUpToAppear();
+
+        Assert.assertEquals(citiesPage.getNewItemInput()
+                        .getAttribute("type"),
+                "text",
+                "New item input 'type' attribute is not 'text'");
+    }
+
+    @Test(priority = 30)
+    public void createNewCity() {
+        String city = "Nikola Zivkovic's city";
+
+        navPage.getAdminLink().click();
+        navPage.getCitiesLink().click();
+
+        citiesPage.getNewItemButton().click();
+        citiesPage.waitForEditPopUpToAppear();
+
+        citiesPage.getNewItemInput().sendKeys(city);
+        citiesPage.getSaveButtonInEditPopUp().click();
+        citiesPage.waitForEditPopUpToDisappear();
+
+        messagePopUpPage.waitForSuccessPopUpToBeVisible();
+        Assert.assertTrue(messagePopUpPage.getMessageTextFromSuccesPopUp()
+                        .getText().contains("Saved successfully"),
+                "The message from pop-up does not equals with expected result");
+    }
+
+    @Test(priority = 40)
+    public void editCity() {
+        String oldCityName = "Nikola Zivkovic's city";
+        String newCityName = "Nikola Zivkovic's city Edited";
+
+        navPage.getAdminLink().click();
+        navPage.getCitiesLink().click();
+
+        citiesPage.getSearchInput().sendKeys(oldCityName);
+        citiesPage.waitForNumOfRowsToBe(1);
+        citiesPage.getEditButtonFromRow(1).click();
+        citiesPage.getNewItemInput().click();
+
+        new Actions(driver)
+                .keyDown(Keys.CONTROL)
+                .sendKeys("a")
+                .sendKeys(Keys.BACK_SPACE)
+                .perform();
+
+        citiesPage.getNewItemInput().sendKeys("");
+        citiesPage.getNewItemInput().sendKeys(newCityName);
+        citiesPage.getSaveButtonInEditPopUp().click();
+
+        messagePopUpPage.waitForSuccessPopUpToBeVisible();
+        Assert.assertTrue(messagePopUpPage.getMessageTextFromSuccesPopUp()
+                        .getText().contains("Saved successfully"),
+                "The message from pop-up does not equals with expected result");
+    }
+
+    @Test(priority = 50)
+    public void searchCity(){
+        String cityName = "Nikola Zivkovic's city Edited";
+
+        navPage.getAdminLink().click();
+        navPage.getCitiesLink().click();
+
+        citiesPage.getSearchInput().sendKeys(cityName);
+        citiesPage.waitForNumOfRowsToBe(1);
+
+        Assert.assertEquals(citiesPage.getCellFromRow(1, 1).getText(),
+                cityName,
+                "The 'Name' column does not equals with expected result");
+    }
+
+    @Test(priority = 60)
+    public void deleteCity(){
+        String cityName = "Nikola Zivkovic's city Edited";
+
+        navPage.getAdminLink().click();
+        navPage.getCitiesLink().click();
+
+        citiesPage.getSearchInput().sendKeys(cityName);
+        citiesPage.waitForNumOfRowsToBe(1);
+
+        Assert.assertEquals(citiesPage.getCellFromRow(1, 1).getText(),
+                cityName,
+                "The 'Name' column does not equals with expected result");
+
+        citiesPage.getDeleteButtonFromRow(1).click();
+        citiesPage.waitForDeletePopUpToAppear();
+        citiesPage.getDeleteButtonInDeletePopUp().click();
+
+        messagePopUpPage.waitForSuccessPopUpToBeVisible();
+        Assert.assertTrue(messagePopUpPage.getMessageTextFromSuccesPopUp()
+                        .getText().contains("Deleted successfully"),
+                "The message from pop-up does not equals with expected result");
+    }
+}
