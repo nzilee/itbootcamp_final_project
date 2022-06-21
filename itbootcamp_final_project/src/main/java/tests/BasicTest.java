33643bdf4ce8e1b90b4f462312ae2c3af9577e95
Nikola Zivkovic <nzivkovic7@gmail.com>
2022-06-21 19:23:47 +0200

    Final Selenium Project - Updated: Basic Test (to screenshot every time a test fails); Added: Screenshots folder

--- itbootcamp_final_project/src/main/java/tests/BasicTest.java
+++ itbootcamp_final_project/src/main/java/tests/BasicTest.java
@@ -1,15 +1,23 @@
 package tests;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
 public abstract class BasicTest {
 
@@ -47,8 +55,20 @@ public abstract class BasicTest {
     }
 
     @AfterMethod
-    public void afterMethod(){
+    public void tearDown(ITestResult result) {
+        String timestamp = new SimpleDateFormat(
+                "hh_mm_ss_dd_MM_yyyy").format(new Date());
 
+        if (result.getStatus() == ITestResult.FAILURE) {
+            TakesScreenshot screen = (TakesScreenshot) driver;
+            File screenshot = screen.getScreenshotAs(OutputType.FILE);
+            try {
+                FileHandler.copy(screenshot, new File(
+                        "src/main/java/Screenshots/TestFailure_" + timestamp + ".png"));
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 
     @AfterClass
